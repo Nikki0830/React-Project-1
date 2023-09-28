acfd7f3c7fcad2665762cbffa20554451a87d66b
Nikki0830 <gniki0830@gmail.com>
2023-09-28 17:40:00 +0530

    ESHOP

---
+++ eshp/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header__logo"></div>
+      <div className="header__search"></div>
+      <div className="header__nav">
+        <div className="nav_-items">
+          <span className="nav__itemLineone">Hello Guest</span>
+          <span className="nav__itemLinetwo">Sign in</span>
+        </div>
+        <div className="nav_-items"></div>
+        <div className="nav_-items"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
