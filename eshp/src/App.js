cd039e55affce6f929385a8a1054bf33e92fc93d
Nikki0830 <gniki0830@gmail.com>
2023-09-28 18:34:23 +0530

    ESHOP

--- eshp/src/App.js
+++ eshp/src/App.js
@@ -1,19 +1,12 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Header from "./components/Header/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <div className="header__logo"></div>
-      <div className="header__search"></div>
-      <div className="header__nav">
-        <div className="nav_-items">
-          <span className="nav__itemLineone">Hello Guest</span>
-          <span className="nav__itemLinetwo">Sign in</span>
-        </div>
-        <div className="nav_-items"></div>
-        <div className="nav_-items"></div>
-      </div>
+    <Header/>
     </div>
   );
 }
---
+++ eshp/src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">eShop</h2>
+      </div>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="nav_-items">
+          <span className="nav__itemLineone">Hello Guest</span>
+          <span className="nav__itemLinetwo">Sign in</span>
+        </div>
+        <div className="nav_-items">
+          <span className="nav__itemLineone">Your</span>
+          <span className="nav__itemLinetwo">Shop</span>
+        </div>
+        <div className="nav_-items">
+          <ShoppingBasketIcon />
+          <span className="nav__itemLinetwo nav__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
